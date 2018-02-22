b986188f3301b56a6ca25e62541c18e8bf1031c9
kauroy1994 <kkroy36@gmail.com>
2018-02-22 15:30:13 -0600

    added comments about what to change

--- tree_learn.py
+++ tree_learn.py
@@ -16,5 +16,13 @@ with open("neg.txt") as f:
     
 clf = GradientBoosting(treeDepth=2,trees=5,sampling_rate=1.0)
 clf.setTargets(["putdown"])
+'''
+modify the learn function to
+accept the java input and remove stuff required
+for the python program
+1. Full stops.
+2. Precomputes.
+3. Any initial paramaters before the facts or modes.
+'''
 clf.learn(facts=facts,pos=pos,neg=neg,bk=bk)
 
